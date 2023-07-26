97560807bac64e7bd3897fc5dea02190e830e9cf
johnson-vicky <137767638+johnson-vicky@users.noreply.github.com>
2023-07-26 11:49:10 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,52 @@
+
+let allMusic = [
+  {
+    name: "Master the Blaster",
+    artist: "Bjorn Surrao",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Let me sing a-Kutty Story",
+    artist: "Vijay",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Antha Kanna Paththakka",
+    artist: "yuvan",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Vaathi Coming",
+    artist: "Arivu,Aniruth",
+    img: "vaathi coming",
+    src: "music-4"
+  },
+  {
+    name: "Polakattum Para Para",
+    artist: "santosh narayanan",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Quit Pannuda",
+    artist: "Anirudh",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Pona pogattum",
+    artist: "Anirudh",
+    img: "music-7",
+    src: "music-7"
+  }, 
+  {
+    name: "kabadi",
+    artist: "vijay,Anirudh",
+    img: "music-8",
+    src: "music-8"
+  },
+
+];
